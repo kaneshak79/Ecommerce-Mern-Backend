84b86c170949ca797033df62da4028cba1c8a7da
Kanesha K <kaneshak79@gmail.com>
2026-07-02 10:04:58 +0530

    Clean up Order.js by removing commented code
    Removed commented-out mongoose order schema code.

--- models/Order.js
+++ models/Order.js
@@ -1,22 +1,5 @@
 
-// import mongoose from "mongoose";
 
-// const orderSchema = new mongoose.Schema(
-//   {
-//     buyer: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//     products: [
-//       {
-//         product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-//         seller: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//         quantity: { type: Number, required: true },
-//         price: { type: Number, required: true }
-//       }
-//     ],
-//     totalAmount: { type: Number, required: true },
-//     shippingAddress: { type: String, required: true },
-//     status: { type: String, default: "pending" } // pending, shipped, delivered, cancelled
-//   },
-//   { timestamps: true }
 // );
 
 // const Order = mongoose.model("Order", orderSchema);
